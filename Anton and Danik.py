6faff8987b9654aa3337d246f1f06f46bbf1268d
Mi01sha01 <midegaurgesa@gmail.com>
2026-02-13 08:58:25 +0300

    Implement solution for Anton and Danik problem

---
+++ Anton and Danik.py
@@ -0,0 +1,17 @@
+def solve():
+  n = int(input())
+  s = input()
+  
+  a = s.count("A")
+  b = s.count("D")
+  
+ 
+  
+  if a == b:
+    print("Friendship")
+  elif a > b:
+    print("Anton")
+  else:
+    print("Danik")
+    
+solve()
