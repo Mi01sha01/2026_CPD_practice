c5ecc340cea69efd8384c068e24a9e54b422e8c5
Mi01sha01 <midegaurgesa@gmail.com>
2026-02-18 15:12:08 +0300

    Create stones on the table.py

---
+++ stones on the table.py
@@ -0,0 +1,11 @@
+def solve():
+        n = int(input())
+        s = input()
+        a = 0
+        for i in range(1,n):
+            if s[i] == s[i-1]:
+                a += 1
+        print(a)        
+            
+        
+solve()        
